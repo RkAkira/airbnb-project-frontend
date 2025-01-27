105848a2201cc4862e4a774175adf5d45018ace4
aryh.mothieu <aryh.mothieu@gmail.com>
2025-01-27 23:08:08 +0100

    migrate source + initialize primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig,  } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura'
 
 
 import { routes } from './app.routes';
@@ -10,7 +12,12 @@ import { authExpired } from './core/auth/auth-expired.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [ 
     provideRouter(routes), 
-    provideAnimations(), 
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }), 
     provideHttpClient(
       withInterceptors([authExpired]),
       withXsrfConfiguration({cookieName: 'XSRF-TOKEN', headerName: 'X-XSRF-TOKEN'})
