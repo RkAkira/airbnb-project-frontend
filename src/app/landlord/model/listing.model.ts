0462579b3a4bf4aecf74cafde0baf254234a47c0
RkAkira <aryh.mothieu@gmail.com>
2025-02-22 00:26:30 +0100

    add listing info page

--- src/app/landlord/model/listing.model.ts
+++ src/app/landlord/model/listing.model.ts
@@ -1,4 +1,5 @@
 import { CategoryName } from "../../layout/navbar/category/category.model";
+import { LandlordListingService } from "../landlord-listing.service";
 import { BathsVO, BedroomsVO,BedsVO, DescriptionVO, GuestsVO, PriceVO, TitleVO } from "./listing-vo.model";
 import { NewListingPicture } from "./picture.model";
 
@@ -41,4 +42,21 @@ export interface CardListing {
     publicId: string,
     loading: boolean
 }
+
+export interface Listing {
+    description: Description,
+    pictures: Array<DisplayPicture>,
+    info: NewListingInfo,
+    price: PriceVO,
+    category: CategoryName,
+    location: string,
+    landlord: LandlordListing
+}
+
+export interface LandlordListing {
+    firstname: string,
+    imageUrl: string
+}
+
+
 
--- src/app/tenant/tenant-listing.service.ts
+++ src/app/tenant/tenant-listing.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { computed, inject, Injectable, signal, WritableSignal } from '@angular/core';
-import { CardListing } from '../landlord/model/listing.model';
+import { CardListing, Listing } from '../landlord/model/listing.model';
 import { State } from '../core/model/state.model';
 import { createPaginationOption, Page, Pagination } from '../core/model/request.model';
 import { CategoryName } from '../layout/navbar/category/category.model';
@@ -16,6 +16,10 @@ export class TenantListingService {
   private getAllByCategory$: WritableSignal<State<Page<CardListing>>> 
   = signal(State.Builder<Page<CardListing>>().forInit());
   getAllByCategorySig = computed(() => this.getAllByCategory$());
+
+  private getOneByPublicId$: WritableSignal<State<Listing>> 
+  = signal(State.Builder<Listing>().forInit());
+  getOneByPublicIdSig = computed(() => this.getOneByPublicId$());
  
   getAllByCategory(category: CategoryName, pageRequest: Pagination ) {
     let params = createPaginationOption(pageRequest);
@@ -32,4 +36,19 @@ export class TenantListingService {
   resetGetAllByCategory() {
     this.getAllByCategory$.set(State.Builder<Page<CardListing>>().forInit());
   }
+
+  getOneByPublicId(publicId: string) {
+    const params = new HttpParams().set("publicId", publicId);
+    this.http.get<Listing>(`${environment.API_URL}/tenant-listing/get-one`, {params})
+    .subscribe({
+      next: listing => { console.log(listing);
+        this.getOneByPublicId$.set(State.Builder<Listing>().forSuccess(listing))},
+      error: error => this.getOneByPublicId$.set(State.Builder<Listing>().forError(error))
+    });
+  }
+
+  resetGetOneByPublicId() {
+    this.getOneByPublicId$.set(State.Builder<Listing>().forInit());
+  }
+    
 }
